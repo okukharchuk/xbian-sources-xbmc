olvidalo/xbmc

OMXImage: Rearchitect to use file buffer

xbmc/TextureCacheJob.cpp
@@ -91,18 +91,6 @@ bool CTextureCacheJob::CacheTexture(CBaseTexture **out_texture)
   else if (m_details.hash == m_oldHash)
     return true;
 
-#if defined(HAS_OMXPLAYER)
-  if (CServiceBroker::GetSettings().GetBool("videoplayer.acceleratedjpegs") && COMXImage::CreateThumb(image, width, height, additional_info, CTextureCache::GetCachedPath(m_cachePath + ".jpg")))
-  {
-    m_details.width = width;
-    m_details.height = height;
-    m_details.file = m_cachePath + ".jpg";
-    if (out_texture)
-      *out_texture = LoadImage(CTextureCache::GetCachedPath(m_details.file), width, height, "" /* already flipped */);
-    CLog::Log(LOGDEBUG, "Fast %s image '%s' to '%s': %p", m_oldHash.empty() ? "Caching" : "Recaching", CURL::GetRedacted(image).c_str(), m_details.file.c_str(), out_texture);
-    return true;
-  }
-#endif
   CBaseTexture *texture = nullptr;
   if (additional_info == "music")
   { // special case for embedded music images
@@ -118,6 +106,28 @@ bool CTextureCacheJob::CacheTexture(CBaseTexture **out_texture)
     if (!(!(file.IsPicture() && !(file.IsZIP() || file.IsRAR() || file.IsCBR() || file.IsCBZ() ))
         && !StringUtils::StartsWithNoCase(file.GetMimeType(), "image/") && !StringUtils::EqualsNoCase(file.GetMimeType(), "application/octet-stream"))) // ignore non-pictures
     {
+#if defined(HAS_OMXPLAYER)
+      if (CServiceBroker::GetSettings().GetBool("videoplayer.acceleratedjpegs"))
+      {
+        // Read image into memory to use our vfs
+        XFILE::CFile xfile;
+        XFILE::auto_buffer buf;
+
+        if (xfile.LoadFile(image, buf) > 0)
+        {
+          if (COMXImage::CreateThumb(image, buf, width, height, additional_info, CTextureCache::GetCachedPath(m_cachePath + ".jpg")))
+          {
+            m_details.width = width;
+            m_details.height = height;
+            m_details.file = m_cachePath + ".jpg";
+            if (out_texture)
+              *out_texture = LoadImage(CTextureCache::GetCachedPath(m_details.file), width, height, "" /* already flipped */);
+            CLog::Log(LOGDEBUG, "Fast %s image '%s' to '%s': %p", m_oldHash.empty() ? "Caching" : "Recaching", CURL::GetRedacted(image).c_str(), m_details.file.c_str(), out_texture);
+            return true;
+          }
+        }
+      }
+#endif
       texture = new CTexture();
       if (!(texture->LoadFromFileInternal(image, width, height, true, file.GetMimeType())))
       {

okukharchuk/xbian-sources-xbmc

xbmc/TextureCacheJob.cpp
@@ -80,20 +80,6 @@ bool CTextureCacheJob::CacheTexture(CBaseTexture **out_texture)
   else if (m_details.hash == m_oldHash)
     return true;
 
-#if defined(TARGET_RASPBERRY_PI)
-  if (CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool("videoplayer.acceleratedjpegs") && COMXImage::CreateThumb(image, width, height, additional_info, CTextureCache::GetCachedPath(m_cachePath + ".jpg")))
-  {
-    m_details.width = width;
-    m_details.height = height;
-    m_details.file = m_cachePath + ".jpg";
-    if (out_texture)
-      *out_texture = LoadImage(CTextureCache::GetCachedPath(m_details.file), width, height, "" /* already flipped */);
-    CLog::Log(LOGDEBUG, "Fast %s image '%s' to '%s': %p",
-              m_oldHash.empty() ? "Caching" : "Recaching", CURL::GetRedacted(image),
-              m_details.file, static_cast<void*>(out_texture));
-    return true;
-  }
-#endif
   CBaseTexture *texture = nullptr;
   if (additional_info == "music")
   { // special case for embedded music images
@@ -109,6 +95,30 @@ bool CTextureCacheJob::CacheTexture(CBaseTexture **out_texture)
     if (!(!(file.IsPicture() && !(file.IsZIP() || file.IsRAR() || file.IsCBR() || file.IsCBZ() ))
         && !StringUtils::StartsWithNoCase(file.GetMimeType(), "image/") && !StringUtils::EqualsNoCase(file.GetMimeType(), "application/octet-stream"))) // ignore non-pictures
     {
+#if defined(TARGET_RASPBERRY_PI)
+      if (CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool("videoplayer.acceleratedjpegs"))
+      {
+        // Read image into memory to use our vfs
+        XFILE::CFile xfile;
+        XFILE::auto_buffer buf;
+
+        if (xfile.LoadFile(image, buf) > 0)
+        {
+          if (COMXImage::CreateThumb(image, buf, width, height, additional_info, CTextureCache::GetCachedPath(m_cachePath + ".jpg")))
+          {
+            m_details.width = width;
+            m_details.height = height;
+            m_details.file = m_cachePath + ".jpg";
+            if (out_texture)
+              *out_texture = LoadImage(CTextureCache::GetCachedPath(m_details.file), width, height, "" /* already flipped */);
+            CLog::Log(LOGDEBUG, "Fast %s image '%s' to '%s': %p",
+                  m_oldHash.empty() ? "Caching" : "Recaching", CURL::GetRedacted(image),
+                  m_details.file, static_cast<void*>(out_texture));
+            return true;
+          }
+        }
+      }
+#endif
       texture = new CTexture();
       if (!(texture->LoadFromFileInternal(image, width, height, true, file.GetMimeType())))
       {